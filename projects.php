440980d07bf28791ec8528eb61c4c711cee25f3c
Akshay <er.akshayone@gmail.com>
2025-08-25 17:01:53 +0530

    changes done by 250825 -1

--- projects.php
+++ projects.php
@@ -83,18 +83,6 @@
 								</div>
 							</div>
 						</li>
-						<li class="card-container col-lg-3 col-md-4 col-sm-6 p-lr0 food wow fadeIn" data-wow-duration="2s" data-wow-delay="0.8s">
-							<div class="dlab-media dlab-img-overlay1 dlab-img-effect portbox1">
-								<img src="images/projects/commercial-4.jpg" alt="">
-								<div class="overlay-bx">
-									<div class="portinner">
-										<span>July 3, 2016 in Travelling</span>
-										<h3 class="port-title"><a href="project-detail.php">Design is where science</a></h3>
-										<a href="project-detail.php" class="btn outline outline-2 radius-xl">View Project</a>
-									</div>
-								</div>
-							</div>
-						</li>
 						<li class="card-container col-lg-3 col-md-4 col-sm-6 p-lr0 mockup technology wow fadeIn" data-wow-duration="2s" data-wow-delay="0.2s">
 							<div class="dlab-media dlab-img-overlay1 dlab-img-effect portbox1">
 								<img src="images/projects/interior-1.jpg" alt="">
